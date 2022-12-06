d19d70a501f1d418a8ff9bd44b1ac1a22b4aa862
crjptpp <119838914+crjptpp@users.noreply.github.com>
2022-12-06 17:26:22 +0900

    Update 7z.ps1

--- sedemo01/7z.ps1
+++ sedemo01/7z.ps1
@@ -1,3 +1,3 @@
-C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://7-zip.org/a/7z2201-x64.exe $env:TEMP\7z.Exe
+C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/crjptpp/tools_public/raw/main/7z2201-x64.exe $env:TEMP\7z.Exe
 Start-Process -FilePath $env:TEMP\7z.Exe -Args "/S" -Verb RunAs -Wait
 Remove-Item $env:TEMP\7z.Exe
