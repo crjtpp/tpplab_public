c74ee6accd457434642a77eed270fd7d87e6dcf2
crjtpp <119838914+crjtpp@users.noreply.github.com>
2022-12-14 13:17:48 +0900

    Update scan.bat

--- sedemo01/scan.bat
+++ sedemo01/scan.bat
@@ -2,5 +2,5 @@
 for /f %%a in ('wmic os get LocalDateTime ^| findstr \.') DO set LDT=%%a
 set filename=%LDT:~0,12%
 
-C:\Windows\system32\cmd.exe /c whoami /groups >> %TEMP%\%filename%_scan.dat & ipconfig /all >> %TEMP%\%filename%_scan.dat & arp -a >> %TEMP%\%filename%_scan.dat & netstat -na >> %TEMP%\%filename%_scan.dat & systeminfo >> %TEMP%\%filename%_scan.dat & netsh advfirewall firewall show rule name=all >> %TEMP%\%filename%_scan.dat
-C:\Windows\system32\cmd.exe /c dir /s "%userprofile%\Documents" /b /a-d >> %TEMP%\%filename%_files.dat
+C:\Windows\system32\cmd.exe /c whoami /groups >> %TEMP%\%filename%_scan.txt & ipconfig /all >> %TEMP%\%filename%_scan.txt & arp -a >> %TEMP%\%filename%_scan.txt & netstat -na >> %TEMP%\%filename%_scan.txt & systeminfo >> %TEMP%\%filename%_scan.txt & netsh advfirewall firewall show rule name=all >> %TEMP%\%filename%_scan.txt
+C:\Windows\system32\cmd.exe /c dir /s "%userprofile%\Documents" /b /a-d >> %TEMP%\%filename%_files.txt
