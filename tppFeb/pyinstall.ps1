6ce4d942f19665d9e424afd2e5d7fea9ed535049
crjtamaki <119922159+crjtamaki@users.noreply.github.com>
2023-02-20 15:18:16 +0900

    Update pyinstall.ps1

--- tppFeb/pyinstall.ps1
+++ tppFeb/pyinstall.ps1
@@ -2,3 +2,4 @@ C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/crjtpp/t
 C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://raw.githubusercontent.com/fortra/impacket/master/examples/psexec.py $env:TEMP\psexec.py
 Start-Process -FilePath $env:TEMP\pythonwinx64.Exe -Args "/S" -Verb RunAs -Wait
 Remove-Item $env:TEMP\pythonwinx64.Exe
+Move-Item $env:temp\psexec.py $env:localappdata\Programs\Python\Python311\
