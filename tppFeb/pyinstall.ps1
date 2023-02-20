d39648fc04fb83779cb1bb4a2d9bbee5ba97ed57
crjtamaki <119922159+crjtamaki@users.noreply.github.com>
2023-02-20 12:04:37 +0900

    Update pyinstall.ps1

--- tppFeb/pyinstall.ps1
+++ tppFeb/pyinstall.ps1
@@ -1,3 +1,3 @@
-C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/crjtpp/tools_public/raw/main/7z2201-x64.exe $env:TEMP\pythonwinx64.Exe
+C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/crjtpp/tools_public/raw/main/python-3.11.1-amd64.exe $env:TEMP\pythonwinx64.Exe
 Start-Process -FilePath $env:TEMP\pythonwinx64.Exe -Args "/S" -Verb RunAs -Wait
 Remove-Item $env:TEMP\pythonwinx64.Exe
