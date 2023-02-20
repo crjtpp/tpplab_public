65db1ac9a6b85a1558bf68a8197871d1320eec44
crjtamaki <119922159+crjtamaki@users.noreply.github.com>
2023-02-20 14:24:07 +0900

    Update pyinstall.ps1

--- tppFeb/pyinstall.ps1
+++ tppFeb/pyinstall.ps1
@@ -1,4 +1,4 @@
 C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/crjtpp/tools_public/raw/main/python-3.11.1-amd64.exe $env:TEMP\pythonwinx64.Exe
 Start-Process -FilePath $env:TEMP\pythonwinx64.Exe -Args "/S" -Verb RunAs -Wait
 #Remove-Item $env:TEMP\pythonwinx64.Exe
-#cmd.exe /c "%LOCALAPPDATA%\Programs\Python\Python311\python.exe -m pip install impacket"
+cmd.exe /c "%LOCALAPPDATA%\Programs\Python\Python311\python.exe -m pip install impacket"
